34a4077b7548480c57b2d59e946186f21f72fe3a
Ahmed Osman <ahmedosmanazrk@gmail.com>
2017-11-10 16:55:01 +0200

    added game logic

---
+++ lib/tictactoe/game.rb
@@ -0,0 +1,106 @@
+module Tictactoe
+
+	class Game
+
+		def initialize(board=nil)
+			@board = board || Board.new
+			@p1 = "X"
+			@p2 = "O"
+			@won = nil
+		end
+
+		def next_move(sign)
+			@turn = @p1 == sign ? @p1 : @p2
+			minimax(@turn)
+		end
+
+		def state
+			state = over?
+			if state == @p1 || state == @p2
+				@won = state
+				1
+			elsif state
+				2
+			else
+				3
+			end
+		end
+
+		private
+		
+		def reset
+			@board.reset
+		end
+
+		def switch_turn
+			@turn = ( @turn == @p1 ? @p2 : @p1 )
+		end
+
+		def play(n)
+			@board.check(@turn, n)
+		end
+
+		def possible_moves
+			@board.empty_cells
+		end
+
+		def won?(sign=@turn)
+
+			@board.diagonals.each do |diagonal|
+				return true if diagonal.all? { |v| v == sign } 
+			end
+			@board.rows.each do |row|
+				return true if row.all? { |v| v == sign }
+			end
+			@board.columns.each do |column|
+				return true if column.all? { |v| v == sign }
+			end
+			false
+		end
+
+		def over?
+			# check if somebody won
+			@board.diagonals.each do |diagonal|
+				return diagonal.first if diagonal.compact.length == 3 && diagonal.uniq.length == 1 
+			end
+			@board.rows.each do |row|
+				return row.first if row.compact.length == 3 && row.uniq.length == 1
+			end
+			@board.columns.each do |column|
+				return column.first if column.compact.length == 3 && column.uniq.length == 1
+			end
+			# check if full and nobody won (ie: draw)
+			@board.full?
+		end
+
+		def minimax(sign, depth=0)
+			
+			return score(depth) if over?
+			
+			scores = []
+			moves = []
+			
+			possible_moves.each do |move|
+				@board.check(sign, move)
+				scores << minimax(sign == @p1 ? @p2 : @p1, depth+1 )
+				moves << move
+				@board.reset(move)
+			end 
+			
+			if sign == @turn
+				max_index = scores.each_with_index.max[1]
+				return depth == 0 ? moves[max_index] : scores[max_index]
+			else
+				return scores.min
+			end
+		
+		end
+
+		def score(depth=0)
+			return 10-depth if won?(@turn)
+			return depth-10 if !won?(@turn)
+			0
+		end
+
+	end
+end
