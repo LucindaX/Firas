843fe608cdf0fc417353b7958d976c6c3e81caea
Ahmed Osman <ahmedosmanazrk@gmail.com>
2017-11-10 15:26:50 +0200

    index page layout

--- lib/app.rb
+++ lib/app.rb
@@ -9,7 +9,12 @@ class App < Sinatra::Base
   
   set :public_folder, "#{root}/public"
 
+  get '/' do
+    send_file File.join(settings.public_folder, 'index.html')
+  end
+
   get '/next_move' do
+  
   end
 
 
